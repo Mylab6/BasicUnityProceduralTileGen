fe5927acb669648ca1739481ee750b0c9b3f9092
keith <dreamihad@gmail.com>
2019-10-12 03:24:11 -0400

    etc

---
+++ Assets/BallNav.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BallNav : MonoBehaviour
+{
+    public Transform goal;
+    public bool manual;
+    public float manualSpeed = 5;
+    void Update()
+    {
+
+
+
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * Time.deltaTime * h * manualSpeed);
+
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * v * manualSpeed);
+
+    }
+
+    void Start()
+    {
+        if (!manual)
+        {
+            goal = GameObject.FindGameObjectWithTag("Goal").transform;
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+        }
+    }
+}
---
+++ Assets/SpawnBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+
+    public Vector3 posOffSet;
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var gameOb = Instantiate(prefab);
+        gameOb.transform.position = transform.position + posOffSet;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
